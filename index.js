f60ded91a2f69e1995775ccd05477b91c3901c8c
jhonasjhones <jhonajhone222@gmail.com>
2023-04-15 14:37:55 -0300

    Instalação de express e feito as configurações iniciais das rotas

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const usuarios = require ('./routes/usuarios')
+
+const app = express();
+app.use (express.json()); //Configuração pra ler em json
+
+app.use('/usuarios', usuarios);
+
+app.listen(3000);
+
